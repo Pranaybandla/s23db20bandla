fb6c875197a588674f4352e9eb291fdfcfce0dfa
Pranaybandla <143150658+Pranaybandla@users.noreply.github.com>
2023-11-15 19:37:51 -0600

    added part 7

--- controllers/university.js
+++ controllers/university.js
@@ -157,3 +157,17 @@ exports.university_delete = async function(req, res) {
             res.send(`{'error': '${err}'}`);     
         } 
     }; 
+
+//part 7
+// Handle a delete one view with id from query 
+exports.university_delete_Page = async function(req, res) {     
+    console.log("Delete view for id "  + req.query.id)     
+    try{         
+        result = await university.findById(req.query.id)         
+        res.render('universitydelete', { title: 'University Delete', toShow: result });     
+    }     
+    catch(err){         
+        res.status(500)         
+        res.send(`{'error': '${err}'}`);     
+    } 
+}; 
